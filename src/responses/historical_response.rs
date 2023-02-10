cbf2c219b2e5ea9b2a4289447ccdc25967ae0fc1
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2023-02-09 23:44:29 -0500

    add serialize to historical response

--- src/responses/historical_response.rs
+++ src/responses/historical_response.rs
@@ -1,30 +1,30 @@
-use serde::Deserialize;
+use serde::{ Serialize, Deserialize };
 use super::weather::Weather;
 
-#[derive(Debug, Deserialize)]
-struct HistoricalData {
+#[derive(Debug, Serialize, Deserialize)]
+pub struct HistoricalData {
     #[serde(alias = "dt")]
-    datetime: i64,
-    sunrise: i64,
-    sunset: i64,
-    temp: f64,
-    feels_like: f64,
-    pressure: i64,
-    humidity: i64,
-    dew_point: f64,
-    uvi: Option<f64>,
-    clouds: i64,
-    visibility: Option<i64>,
-    wind_speed: f64,
-    wind_deg: i64,
-    weather: Vec<Weather>
+    pub datetime: i64,
+    pub sunrise: i64,
+    pub sunset: i64,
+    pub temp: f64,
+    pub feels_like: f64,
+    pub pressure: i64,
+    pub humidity: i64,
+    pub dew_point: f64,
+    pub uvi: Option<f64>,
+    pub clouds: i64,
+    pub visibility: Option<i64>,
+    pub wind_speed: f64,
+    pub wind_deg: i64,
+    pub weather: Vec<Weather>
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct HistoricalResponse {
-    lat: f64,
-    lon: f64,
-    timezone: String,
-    timezone_offset: i64,
-    data: Vec<HistoricalData>,
+    pub lat: f64,
+    pub lon: f64,
+    pub timezone: String,
+    pub timezone_offset: i64,
+    pub data: Vec<HistoricalData>,
 }

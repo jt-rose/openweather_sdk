89d5357fe4a96cc577e865684930571d1931fc5f
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2023-02-09 23:46:33 -0500

    update type signatures

--- src/openweather.rs
+++ src/openweather.rs
@@ -1,27 +1,27 @@
+use crate::air_pollution::air_pollution::AirPollution;
 use crate::units::Units;
 use crate::languages::Language;
 use crate::onecall::onecall::OneCall;
 use crate::forecast::forecast::Forecast;
+use crate::geocoding::geocoding::Geocoding;
 use crate::maps::maps::Maps;
 
 pub struct OpenWeather {
-    api_key: &'static str,
-    units: Units,
-    language: Language,
     pub onecall: OneCall,
     pub forecast: Forecast,
-    pub maps: Maps
+    pub maps: Maps,
+    pub air_pollution: AirPollution,
+    pub geocoding: Geocoding
 }
 
 impl OpenWeather {
-    pub fn new(api_key: &'static str, units: Units, language: Language) -> Self {
+    pub fn new(api_key: String, units: Units, language: Language) -> Self {
         Self {
-            api_key,
-            units,
-            language,
-            onecall: OneCall::new(api_key, units, language),
-            forecast: Forecast::new(api_key, units, language),
-            maps: Maps::new(api_key, units, language)
+            onecall: OneCall::new(api_key.clone(), units, language),
+            forecast: Forecast::new(api_key.clone(), units, language),
+            maps: Maps::new(api_key.clone()),
+            air_pollution: AirPollution::new(api_key.clone()),
+            geocoding: Geocoding::new(api_key.clone())
         }
     }
 }

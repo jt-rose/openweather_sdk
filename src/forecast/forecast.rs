10f2e61241793500bde02861db3257c4dc767a7c
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2023-02-09 23:41:29 -0500

    update forecast queries

--- src/forecast/forecast.rs
+++ src/forecast/forecast.rs
@@ -1,19 +1,16 @@
-use std::fmt;
-use std::fmt::{Display, Error};
 use reqwest;
-use serde::de;
 use super::forecast_response::ForecastResponse;
 use crate::units::Units;
 use crate::languages::Language;
 
 pub struct Forecast {
-    api_key: &'static str,
+    api_key: String,
     units: Units,
     language: Language,
 }
 
 impl Forecast {
-    pub fn new(api_key: &'static str, units: Units, language: Language) -> Self {
+    pub fn new(api_key: String, units: Units, language: Language) -> Self {
         Self {
             api_key,
             units,
@@ -23,11 +20,12 @@ impl Forecast {
 
     fn format_query(&self, lat: f64, lon: f64, forecast_type: &str, count: u8) -> String {
         format!(
-            "https://api.openweathermap.org/data/2.5/forecast{}?lat={}&lon={}&cnt={}&lang={}&appid={}",
+            "https://api.openweathermap.org/data/2.5/forecast{}?lat={}&lon={}&cnt={}&units={}&lang={}&appid={}",
             forecast_type,
             lat,
             lon,
             count,
+            self.units,
             self.language,
             self.api_key)
     }
@@ -44,8 +42,8 @@ impl Forecast {
     }
 
     // TODO: confirm response type for hourly, daily, and climate
-    // pub async fn get_hourly_forecast(&self, lat: f64,lon: f64) -> Result<ForecastResponse, Box<dyn std::error::Error>> {
-    //     let url = self.format_query(lat, lon, "/hourly");
+    // pub async fn get_hourly_forecast(&self, lat: f64,lon: f64, count: u8) -> Result<ForecastResponse, Box<dyn std::error::Error>> {
+    //     let url = self.format_query(lat, lon, "/hourly", count);
     //     let resp = reqwest::get(url)
     //         .await?
     //         .json::<ForecastResponse>()
@@ -54,8 +52,8 @@ impl Forecast {
     //     Ok(resp)
     // }
     //
-    // pub async fn get_daily_forecast(&self, lat: f64,lon: f64) -> Result<ForecastResponse, Box<dyn std::error::Error>> {
-    //     let url = self.format_query(lat, lon, "/daily");
+    // pub async fn get_daily_forecast(&self, lat: f64,lon: f64, count: u8) -> Result<ForecastResponse, Box<dyn std::error::Error>> {
+    //     let url = self.format_query(lat, lon, "/daily", count);
     //     let resp = reqwest::get(url)
     //         .await?
     //         .json::<ForecastResponse>()
@@ -64,8 +62,8 @@ impl Forecast {
     //     Ok(resp)
     // }
     //
-    // pub async fn get_climate_forecast(&self, lat: f64,lon: f64) -> Result<ForecastResponse, Box<dyn std::error::Error>> {
-    //     let url = self.format_query(lat, lon, "/climate");
+    // pub async fn get_climate_forecast(&self, lat: f64,lon: f64, count: u8) -> Result<ForecastResponse, Box<dyn std::error::Error>> {
+    //     let url = self.format_query(lat, lon, "/climate", count);
     //     let resp = reqwest::get(url)
     //         .await?
     //         .json::<ForecastResponse>()
--- src/forecast/forecast_response.rs
+++ src/forecast/forecast_response.rs
@@ -1,81 +1,81 @@
-use serde::Deserialize;
+use serde::{ Serialize, Deserialize};
 use crate::responses::weather::Weather;
 
-#[derive(Debug, Deserialize)]
-struct Wind {
-    speed: f64,
-    deg: i64,
-    gust: f64
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Wind {
+    pub speed: f64,
+    pub deg: i64,
+    pub gust: f64
 }
 
-#[derive(Debug, Deserialize)]
-struct Clouds {
-    all: i64
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Clouds {
+    pub all: i64
 }
 
-#[derive(Debug, Deserialize)]
-struct Rain {
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Rain {
     #[serde(alias = "3h")]
-    volume_over_three_hours: f64
+    pub volume_over_three_hours: f64
 }
 
-#[derive(Debug, Deserialize)]
-struct Sys {
-    pod: String
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Sys {
+    pub pod: String
 }
 
-#[derive(Debug, Deserialize)]
-struct Main {
-    temp: f64,
-    feels_like: f64,
-    temp_min: f64,
-    temp_max: f64,
-    pressure: u64,
-    sea_level: i64,
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Main {
+    pub temp: f64,
+    pub feels_like: f64,
+    pub temp_min: f64,
+    pub temp_max: f64,
+    pub pressure: u64,
+    pub sea_level: i64,
     #[serde(alias = "grnd_level")]
-    ground_level: i64,
-    humidity: u64,
-    temp_kf: f64
+    pub ground_level: i64,
+    pub humidity: u64,
+    pub temp_kf: f64
 }
 
-#[derive(Debug, Deserialize)]
-struct ForecastDescription {
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ForecastDescription {
     #[serde(alias = "dt")]
-    datetime: u64,
-    main: Main,
-    weather: Vec<Weather>,
-    clouds: Clouds,
-    wind: Wind,
-    visibility: i64,
-    pop: f64,
-    rain: Option<Rain>,
-    sys: Sys,
-    dt_txt: String
+    pub datetime: u64,
+    pub main: Main,
+    pub weather: Vec<Weather>,
+    pub clouds: Clouds,
+    pub wind: Wind,
+    pub visibility: i64,
+    pub pop: f64,
+    pub rain: Option<Rain>,
+    pub sys: Sys,
+    pub dt_txt: String
 }
 
-#[derive(Debug, Deserialize)]
-struct City {
-    id: u64,
-    name: String,
-    coord: Coord,
-    country: String,
-    population: u64,
-    timezone: i64,
-    sunrise: u64,
-    sunset: u64
+#[derive(Debug, Serialize, Deserialize)]
+pub struct City {
+    pub id: u64,
+    pub name: String,
+    pub coord: Coord,
+    pub country: String,
+    pub population: u64,
+    pub timezone: i64,
+    pub sunrise: u64,
+    pub sunset: u64
 }
 
-#[derive(Debug, Deserialize)]
-struct Coord {
-    lat: f64,
-    lon: f64
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Coord {
+    pub lat: f64,
+    pub lon: f64
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct ForecastResponse {
-    cod: String,
-    message: i64,
-    cnt: i64,
-    list: Vec<ForecastDescription>,
-    city: City,
+    pub cod: String,
+    pub message: i64,
+    pub cnt: i64,
+    pub list: Vec<ForecastDescription>,
+    pub city: City,
 }
